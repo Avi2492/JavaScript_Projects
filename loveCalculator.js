e474d4093bd3f4bb6a8a009c9e11f25c7b8a4b23
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2022-12-30 12:15:19 +0530

    Update loveCalculator.js

--- loveCalculator.js
+++ loveCalculator.js
@@ -3,4 +3,10 @@ prompt("What is your partners name ?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-alert("Your loveScore is " + loveScore + "%.");
+
+if (loveScore == 90) {
+  alert("Your loveScore is " + loveScore + "%. You love each other like jal bin machli");
+}
+else {
+  alert("Your loveScore is " + loveScore + "%.");
+}
